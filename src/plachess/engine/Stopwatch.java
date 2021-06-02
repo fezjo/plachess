6b21e9731de95c8a10cb7a7338305db6fb9bd655
Jozef Číž <jozef.ciz.ml@gmail.com>
2021-06-02 22:53:28 +0200

    Stopwatch

---
+++ src/plachess/engine/Stopwatch.java
@@ -0,0 +1,36 @@
+package plachess.engine;
+
+public class Stopwatch {
+    public String message;
+    public long tStart, tFinish;
+
+    public Stopwatch() {
+        start();
+    }
+
+    public Stopwatch(String message) {
+        this.message = message;
+        start();
+    }
+
+    public long start() {
+        return tStart = System.currentTimeMillis();
+    }
+
+    public long finish() {
+        return tFinish = System.currentTimeMillis();
+    }
+
+    public long finish(boolean print) {
+        long result = finish();
+        if(print)
+            print();
+        return result;
+    }
+
+    public long print() {
+        long duration = tFinish - tStart;
+        System.out.printf(message, duration);
+        return duration;
+    }
+}
